096575a9fcf23e1b7f9b9136438db21666480b83
Ismayil <ismayilovismayil1226@gmail.com>
2022-04-09 17:43:10 +0400

    Session1 - label added

--- Session1/ViewController.swift
+++ Session1/ViewController.swift
@@ -11,7 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .darkGray
+        print("Height: \(self.view.frame.height)")
+        print("Width: \(self.view.frame.width)")
+        
+        let myLabel = UILabel()
+        myLabel.frame = CGRect(x: 170, y: 400, width: 80, height: 30)
+        myLabel.text = "Test1"
+        myLabel.textColor = .orange
+        myLabel.backgroundColor = .lightGray
+        myLabel.textAlignment = .center
+        myLabel.font = .boldSystemFont(ofSize: 30)
+        self.view.addSubview(myLabel)
+        
+       
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Did appear")
     }
 
 
